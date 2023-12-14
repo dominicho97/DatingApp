c97df29ede6928105c6a79927600690bce5cf761
HoDominic <dominic.ho97@gmail.com>
2023-12-14 17:32:01 +0100

    added accountcontroller 37

---
+++ API/Data/Migrations/20231214163035_UserPasswordAdded2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPasswordAdded2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalte",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<byte>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(byte[]),
+                oldType: "BLOB",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte>(
+                name: "PasswordSalte",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,9 +6,9 @@
         public int Id { get; set; }
         public string UserName { get; set; }
 
-        public byte PasswordHash { get; set; }
+        public byte[] PasswordHash { get; set; }
 
-        public byte PasswordSalte { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
         
 
