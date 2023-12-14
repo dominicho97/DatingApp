58b5fe303f3f427780b8a6d78bbbb24c2fa47345
HoDominic <dominic.ho97@gmail.com>
2023-12-14 23:59:44 +0100

    Added LoginDto

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities{
+
+
+    public class LoginDto{
+
+     
+        public string Username { get; set; }
+
+       
+        public string Password { get; set; }
+
+
+    }
+
+
+
+
+}
