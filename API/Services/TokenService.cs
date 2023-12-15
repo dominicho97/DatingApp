f7b75e8ee83b3f0ffdffefc534fc7bc0c3b8a729
dominicho97 <dominic.ho97@gmail.com>
2023-12-15 13:45:01 +0100

    added project to new repository

---
+++ API/Services/TokenService.cs
@@ -0,0 +1,16 @@
+using API.Entities;
+using API.Interfaces;
+
+namespace API.Services{
+
+
+    public class TokenService : ITokenService
+    {
+        public string CreateToken(AppUser user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
