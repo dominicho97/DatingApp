774548fc1e75b3986e91374dedd5a01540fefbbb
dominicho97 <dominic.ho97@gmail.com>
2023-12-20 15:46:42 +0100

    Finished error handling -section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,6 @@
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using API.MiddleWare;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +15,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseMiddleware<ExceptionMiddleWare>();
 
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
